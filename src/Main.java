ffb9e2acffb3e2b97bfd0a50d971713b6ce974b8
Siarhei Arlouski <siarhei.arlouski@gmail.com>
2022-11-07 21:24:33 +0300

    Added exceptions

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
-import Tools.Hammer;
 import Tools.Tools;
+
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Tools tools = new Tools();
         tools.toolsCost();
+
+        System.out.println("Enter the number of tool to purchase: ");
+        Scanner scanner = new Scanner(System.in);
+        double numberOfTool = scanner.nextDouble();
+
+        if (numberOfTool <= 0 || numberOfTool > 8) {
+            try {
+                throw new IOException();
+            } catch (IOException e) {
+                System.out.println("Sorry, incorrect number. Try again");
+            }
+        } else {
+            System.out.println("Thank you!");
+        }
     }
 }
--- src/Tools/Tools.java
+++ src/Tools/Tools.java
@@ -10,14 +10,14 @@ public class Tools implements ToolsCost{
         SalvagedHammer salvagedHammer = new SalvagedHammer();
         SalvagedAxe salvagedAxe = new SalvagedAxe();
         SalvagedIcepick salvagedIcepick = new SalvagedIcepick();
-        System.out.println("TOOLS COST: \n" +
-                "Hammer - " + hammer.hammerCost() + " scraps;\n" +
-                "Stone Hatchet - " + stoneHatchet.stoneHatchetCost() + " scraps;\n" +
-                "Stone Pickaxe - " + stonePickaxe.stonePickaxeCost() + " scraps;\n" +
-                "Metal Hatchet - " + metalHatchet.metalHatchetCost() + " scraps;\n" +
-                "Metal Pickaxe - " + metalPickaxe.metalPickaxeCost() + " scraps;\n" +
-                "Salvaged Hammer - " + salvagedHammer.salvagedHammerCost() + " scraps;\n" +
-                "Salvaged Axe - " + salvagedAxe.salvagedAxeCost() + " scraps;\n" +
-                "Salvaged Icepick - " + salvagedIcepick.salvagedIcepickCost() + " scraps.");
+        System.out.println("Tools Cost: \n\n" +
+                "1. Hammer - " + hammer.hammerCost() + " scraps;\n" +
+                "2. Stone Hatchet - " + stoneHatchet.stoneHatchetCost() + " scraps;\n" +
+                "3. Stone Pickaxe - " + stonePickaxe.stonePickaxeCost() + " scraps;\n" +
+                "4. Metal Hatchet - " + metalHatchet.metalHatchetCost() + " scraps;\n" +
+                "5. Metal Pickaxe - " + metalPickaxe.metalPickaxeCost() + " scraps;\n" +
+                "6. Salvaged Hammer - " + salvagedHammer.salvagedHammerCost() + " scraps;\n" +
+                "7. Salvaged Axe - " + salvagedAxe.salvagedAxeCost() + " scraps;\n" +
+                "8. Salvaged Icepick - " + salvagedIcepick.salvagedIcepickCost() + " scraps.\n");
     }
 }
