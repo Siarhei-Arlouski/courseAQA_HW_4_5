695d736ba5564c5099d40bf7ab7d15bc82a60e7f
Siarhei Arlouski <siarhei.arlouski@gmail.com>
2022-11-20 23:09:03 +0300

    Added unit tests

---
+++ src/UnitTest.java
@@ -0,0 +1,65 @@
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import Resources.Resources;
+import Tools.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UnitTest {
+    Hammer hammer = new Hammer();
+    StoneHatchet stoneHatchet = new StoneHatchet();
+    StonePickaxe stonePickaxe = new StonePickaxe();
+    MetalHatchet metalHatchet = new MetalHatchet();
+    MetalPickaxe metalPickaxe = new MetalPickaxe();
+    SalvagedHammer salvagedHammer = new SalvagedHammer();
+    SalvagedAxe salvagedAxe = new SalvagedAxe();
+    SalvagedIcepick salvagedIcepick = new SalvagedIcepick();
+
+    @Test
+    void resourcesCalculatingTest() {
+        Resources resources = new Resources(100,200,300,5,5);
+        double sum = resources.getWood() + resources.getStone() + resources.getMetalFragments() + resources.getMetalPipe() + resources.getMetalBlade();
+        assertEquals(267, sum);
+    }
+
+    @Test
+    void hammerCostTest() {
+        assertEquals(2, hammer.hammerCost());
+    }
+
+    @Test
+    void stoneHatchetCostTest() {
+        assertEquals(9, stoneHatchet.stoneHatchetCost());
+    }
+
+    @Test
+    void stonePickaxeCostTest() {
+        assertEquals(9, stonePickaxe.stonePickaxeCost());
+    }
+
+    @Test
+    void metalHatchetCostTest() {
+        assertEquals(9.5, metalHatchet.metalHatchetCost());
+    }
+
+    @Test
+    void metalPickaxeCostTest() {
+        assertEquals(14.5, metalPickaxe.metalPickaxeCost());
+    }
+
+    @Test
+    void salvagedHammerCostTest() {
+        assertEquals(35, salvagedHammer.salvagedHammerCost());
+    }
+
+    @Test
+    void salvagedAxeCostTest() {
+        assertEquals(105, salvagedAxe.salvagedAxeCost());
+    }
+
+    @Test
+    void salvagedIcepickCostTest() {
+        assertEquals(105, salvagedIcepick.salvagedIcepickCost());
+    }
+}
